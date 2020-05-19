c633513c730a0325215993c8b759e157f7fb5394
Robert Olson <olson@mcs.anl.gov>
2020-05-19 12:16:52 -0500

    add missing file.

---
+++ lib/Bio/P3/SARS2Assembly.pm
@@ -0,0 +1,68 @@
+package Bio::P3::SARS2Assembly;
+
+#
+# Mostly empty package; used as a placeholder 
+#
+
+use strict;
+use Module::Metadata;
+use IPC::Run 'run';
+use Data::Dumper;
+
+use base 'Exporter';
+
+our @EXPORT_OK = qw(reference_fasta_path primer_bedpe_path run_cmds);
+
+our $ReferenceFasta = "MN908947.fasta";
+our $PrimerBedpe = "SC2_200324.bedpe";
+our $ArticSchemes = "primer_schemes";
+
+sub reference_fasta_path
+{
+    my $mpath = Module::Metadata->find_module_by_name("Bio::P3::SARS2Assembly");
+    $mpath =~ s/\.pm$//;
+
+    my $ref = "$mpath/$ReferenceFasta";
+    return $ref;
+}
+
+sub primer_bedpe_path
+{
+    my $mpath = Module::Metadata->find_module_by_name("Bio::P3::SARS2Assembly");
+    $mpath =~ s/\.pm$//;
+
+    my $ref = "$mpath/$PrimerBedpe";
+    return $ref;
+}
+
+sub artic_primer_schemes_path
+{
+    my $mpath = Module::Metadata->find_module_by_name("Bio::P3::SARS2Assembly");
+    $mpath =~ s/\.pm$//;
+
+    my $ref = "$mpath/$ArticSchemes";
+    return $ref;
+}
+
+sub run_cmds
+{
+    my(@cmds) = @_;
+
+    print STDERR "Execute:\n";
+    for my $c (@cmds)
+    {
+	if (ref($c) eq 'ARRAY')
+	{
+	    print STDERR "\t@$c\n";
+	}
+	elsif(!ref($c))
+	{
+	    print STDERR "\t$c\n";
+	}
+    }
+
+    my $ok = run(@cmds);
+    $ok or die "Failed running pipeline: \n" . Dumper(\@cmds);
+}
+
+1;
