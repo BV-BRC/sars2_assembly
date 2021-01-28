0357c438311fbdf505e3684046e404bc65f4fdf7
Robert Olson <olson@mcs.anl.gov>
2021-01-28 12:36:13 -0600

    Change batching

--- service-scripts/bebop-run-chunk.pl
+++ service-scripts/bebop-run-chunk.pl
@@ -1,6 +1,11 @@
 #
 # Run a chunk of jobs on a bebop node.
 #
+# We expect to be run as an array job with a stepsize of 1.
+# We run entries-per-job SRA entries in each job. This will be spread
+# across the 36 cores of the node. 
+#
+# Former:
 # We look up the data based on the SLURM_ARRAY_TASK_ID
 # and SLURM_ARRAY_TASK_STEP values; that is, we will
 # compute on items task_id thru (task_id + task_step - 1)
@@ -17,23 +22,27 @@
 use strict;
 use Data::Dumper;
 use File::Path 'make_path';
+use Proc::ParallelLoop;
+use Time::HiRes 'gettimeofday';
+use IPC::Run 'run';
 
-@ARGV == 3 or die "Usage: $0 job-offset sra-def-file output-dir\n";
+@ARGV == 4 or die "Usage: $0 job-offset entries-per-job sra-def-file output-dir\n";
 
 my $job_offset = shift;
+my $entries_per_job = shift;
 my $sra_defs = shift;
 my $output = shift;
 
-my $scratch = "/scratch";
+my $scratch = $ENV{SCRATCH_DIR} // "/scratch";
 
 $job_offset =~ /^\d+$/ or die "Invalid job offset '$job_offset'\n";
 -d $output or die "Output directory $output does not exist\n";
 
-my $start = $job_offset + $ENV{SLURM_ARRAY_TASK_ID};
-my $end = $start + $ENV{SLURM_ARRAY_TASK_STEP} - 1;
+my $start = $job_offset + ($ENV{SLURM_ARRAY_TASK_ID} - 1) * $entries_per_job + 1;;
+my $end = $start + $entries_per_job - 1;
 
-my $threads = int(36 / $ENV{SLURM_ARRAY_TASK_STEP});
-$threads = 4 if $threads > 4;
+my $workers = 9;
+my $threads = 4;
 
 print STDERR "Run from $start to $end\n";
 
@@ -53,35 +62,66 @@ close(S);
 
 print STDERR "Run " . Dumper(\@to_run);
     
-my @pids;
+print STDERR "Start downloads\n";
 
-for my $item (@to_run)
-{
-    my $pid = fork;
-    if ($pid == 0)
+pareach \@to_run, sub {
+    my($ent) = @_;
+    my($id, $sra) = @$ent;
+
+    eval {
+	dl_one($id, $sra);
+    };
+    if ($@)
     {
-	run_one(@$item);
-	exit 0;
+	warn "Error downloading $id $sra: $@";
     }
-    push(@pids, $pid);
-}
+    
+}, { Max_Workers => 2 };
 
-for my $pid (@pids)
-{
-    print STDERR "Wait for $pid\n";
-    my $rc = waitpid($pid, 0);
-    print  STDERR "Pid $pid is done with $?\n";
-}
+print STDERR "Start runs\n";
 
-sub run_one
+pareach \@to_run, sub {
+    my($ent) = @_;
+    my($id, $sra) = @$ent;
+
+    eval {
+	run_one($id, $sra);
+    };
+    if ($@)
+    {
+	warn "Error running $id $sra: $@";
+    }
+    
+}, { Max_Workers => $threads };
+
+
+sub dl_one
 {
     my($id, $sra) = @_;
     $ENV{TMPDIR} = $scratch;
     print STDERR "Running $id: $sra\n";
     my $tmp = "$scratch/task-$id";
     make_path($tmp);
-    my $rc = system("p3-sra", "--id", $sra, "--out", $tmp);
-    $rc == 0 or die "p3_sra failed with $rc for $sra\n";
+
+    my $l2 = substr($sra, 0, 7);
+    my $out = "$output/$l2/$sra";
+    make_path($out);
+
+    my $ok = run(["p3-sra", "--id", $sra, "--out", $tmp],
+		 ">", "$out/download.stdout",
+		 "2>", "$out/download.sterr");
+    $ok or die "p3_sra failed with $? for $sra\n";
+}
+
+sub run_one
+{
+    my($id, $sra) = @_;
+    my $start = gettimeofday;
+    $ENV{TMPDIR} = $scratch;
+    print STDERR "Running $id: $sra\n";
+    my $tmp = "$scratch/task-$id";
+    make_path($tmp);
+
     my $l2 = substr($sra, 0, 7);
     my $out = "$output/$l2/$sra";
     make_path($out);
@@ -94,5 +134,15 @@ sub run_one
     }
     my @cmd = ("sars2-onecodex", @fastq, $sra, $out, "--threads", $threads);
     print STDERR "Run in pid $$ for id $id $sra: @cmd\n";
-    $rc = system(@cmd);
+    my $ok = run(\@cmd,
+	      ">", "$out/assemble.stdout",
+	      "2>", "$out/assemble.sterr");
+    $ok or warn "Assemble failed with $?\n";
+    my $end = gettimeofday;
+    my $elap = $end - $start;
+    if (open(F, ">", "$out/RUNTIME"))
+    {
+	print F "$start\t$end\t$elap\n";
+	close(F);
+    }
 }
